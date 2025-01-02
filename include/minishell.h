e5975f9d41cfc50df2562c42c6263a16431577a8
KHR416 <hyy741@naver.com>
2025-01-02 17:48:55 +0900

    added `struct s_msvar`

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wchoe <wchoe@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 23:00:14 by wchoe             #+#    #+#             */
-/*   Updated: 2024/12/29 23:01:42 by wchoe            ###   ########.fr       */
+/*   Updated: 2025/01/02 17:48:34 by wchoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE	1024
 # endif	// BUFFER_SIZE
+
+typedef struct s_msvar
+{
+	char	**envp;
+}	t_msvar;
 
 char	*ms_getcwd(void);
 #endif	// MINISHELL_H
