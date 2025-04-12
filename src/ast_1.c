2b6f22e276c351705e617d392191e2c48f037d67
KHR416 <hyy741@naver.com>
2025-04-12 17:28:50 +0900

    modified `op_cmp`

--- src/ast_1.c
+++ src/ast_1.c
@@ -6,7 +6,7 @@
 /*   By: wchoe <wchoe@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:10:31 by wchoe             #+#    #+#             */
-/*   Updated: 2025/04/11 15:21:22 by wchoe            ###   ########.fr       */
+/*   Updated: 2025/04/12 17:28:04 by wchoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,20 @@ int	is_operator_node(t_node_type type)
 	return (type == NODE_PIPE || type == NODE_AND || type == NODE_OR);
 }
 
+int	get_operator_precendence(t_node_type type)
+{
+	if (type == NODE_PARANTHESIS_OPEN)
+		return (0);
+	if (type == NODE_AND || type == NODE_OR)
+		return (1);
+	if (type == NODE_PIPE)
+		return (2);
+	return (-1);
+}
+
 int	op_cmp(t_node_type type1, t_node_type type2)
 {
-	if (type1 == type2
-		|| ((type1 == NODE_AND || type1 == NODE_OR)
-		&& (type2 == NODE_AND || type2 == NODE_OR)))
-		return (0);
-	if (type1 == NODE_PIPE && (type2 == NODE_AND || type2 == NODE_OR))
-		return (1);
-	return (-1);
+	return (get_operator_precendence(type1) - get_operator_precendence(type2));
 }
 
 int	cmp_not_same(t_node_type type, t_node_type type_open)
